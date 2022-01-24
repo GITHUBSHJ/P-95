b55d4ef3fa8be8cf32538bf4b4eec49a885b5b3f
GITHUBSHJ <89509745+GITHUBSHJ@users.noreply.github.com>
2022-01-24 19:09:30 +0530

    Add files via upload

---
+++ chat_room_main.js
@@ -0,0 +1,9 @@
+coding_next_part = document.getElementById("coding_next_part").value;
+
+localStorage.setItem("coding_next_part", coding_next_part);
+
+console.log("i will do the coding next class. byeee");
+
+
+
+
